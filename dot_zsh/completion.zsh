1113ea45d2ed8d49642fe57a0468d7e477998789
bellorr <9314085+bellorr@users.noreply.github.com>
2026-02-02 21:27:17 -0500

    Update .zsh/completion.zsh

--- dot_zsh/completion.zsh
+++ dot_zsh/completion.zsh
@@ -1,7 +1,14 @@
 # Managed by chezmoi - Edit with: chezmoi edit ~/.zsh/completion.zsh
 
 #### Completion Engine ####
-autoload -Uz +X compinit && compinit
+
+# Homebrew completions (must be before compinit)
+fpath=($(brew --prefix)/share/zsh/site-functions $fpath)
+
+autoload -Uz compinit
+compinit
+
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 zstyle ':completion:*' menu select
+
 compdef kubecolor=kubectl
